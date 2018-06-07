62df419a29465cf7cf41cd72c7ddbd6244fdf0aa
DaltonHart <daltonvaren@gmail.com>
2018-06-07 11:18:21 -0700

    made posting system

--- scribble_project/scribble/urls.py
+++ scribble_project/scribble/urls.py
@@ -5,5 +5,12 @@ urlpatterns = [
     # if we go to '/' got to the views, run the post list function
     path('', views.post_list, name='post_list'),
     path('posts/<int:pk>', views.post_detail, name='post_detail'),
-    path('comments/', views.comment_list, name='comment_list')
+    path('comments/<int:pk>', views.comment_detail, name='comment_detail'),
+    path('comments/', views.comment_list, name='comment_list'),
+    path('posts/new', views.post_create, name='post_create'),
+    path('comments/new', views.comment_create, name='comment_create'),
+    path('posts/<int:pk>/edit', views.post_edit, name='post_edit'),
+    path('comments/<int:pk>/edit', views.comment_edit, name='comment_edit'),
+    path('posts/<int:pk>/delete', views.post_delete, name='post_delete'),
+    path('comments/<int:pk>/delete', views.comment_delete, name='comment_delete'),
 ]
--- scribble_project/scribble/views.py
+++ scribble_project/scribble/views.py
@@ -1,19 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
-# Create your views here.
+
+#post index
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'scribble/post_list.html', {'posts': posts})
-
+#post  show
 def post_detail(request, pk):
     post = Post.objects.get(id=pk)
     return render(request, 'scribble/post_detail.html', {'post': post})
-
+#comment index
 def comment_list(request):
     comments = Comment.objects.all()
     return render(request, 'scribble/comment_list.html',{'comments': comments})
-
+#comment show
 def comment_detail(request, pk):
-    post = Comment.objects.get(id=pk)
+    comment = Comment.objects.get(id=pk)
     return render(request, 'scribble/comment_detail.html', {'comment': comment})
+
+#create
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'scribble/post_form.html', {'form': form})
+
+def comment_create(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('comment_detail', pk=comment.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'scribble/comment_form.html', {'form': form})
+
+#Update
+def post_edit(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'scribble/post_form.html', {'form': form})
+
+def comment_edit(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('comment_detail', pk=comment.pk)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'scribble/comment_form.html', {'form': form})
+
+#destory 
+def post_delete(request, pk):
+    Post.objects.get(id=pk).delete()
+    return redirect('post_list')
+
+def comment_delete(request, pk):
+    Comment.objects.get(id=pk).delete()
+    return redirect('comment_list')
